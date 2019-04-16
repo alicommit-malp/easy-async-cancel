efa7fe626da1062e6c91ec0f459d2a7617510f12
Ali Tab <ali.tabryzy@gmail.com>
2019-04-16 13:52:56 +0300

    operation cancel exception custom message feature added

--- src/easyAsyncCancel/TaskCancellationExtension.cs
+++ src/easyAsyncCancel/TaskCancellationExtension.cs
@@ -27,6 +27,27 @@ namespace easyAsyncCancel
 
 
         /// <summary>
+        /// add cancellation functionality to Task T with exception message 
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="cancellationToken"></param>
+        /// <param name="message"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="OperationCanceledException"></exception>
+        public static async Task<T> CancelAfter<T>(
+            this Task<T> task, CancellationToken cancellationToken,string message)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            using (cancellationToken.Register(
+                s => ((TaskCompletionSource<bool>) s).TrySetResult(true), tcs))
+                if (task != await Task.WhenAny(task, tcs.Task))
+                    throw new OperationCanceledException(message,cancellationToken);
+            return await task;
+        }
+
+
+        /// <summary>
         /// add cancellation functionality to Tasks 
         /// </summary>
         /// <param name="task"></param>
@@ -42,6 +63,27 @@ namespace easyAsyncCancel
                 s => ((TaskCompletionSource<bool>) s).TrySetResult(true), tcs))
                 if (task != await Task.WhenAny(task, tcs.Task))
                     throw new OperationCanceledException(cancellationToken);
+            await task;
+        }
+
+
+        /// <summary>
+        /// add cancellation functionality to Tasks with exception message 
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="cancellationToken"></param>
+        /// <param name="message"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="OperationCanceledException"></exception>
+        public static async Task CancelAfter(
+            this Task task, CancellationToken cancellationToken,string message)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            using (cancellationToken.Register(
+                s => ((TaskCompletionSource<bool>) s).TrySetResult(true), tcs))
+                if (task != await Task.WhenAny(task, tcs.Task))
+                    throw new OperationCanceledException(message,cancellationToken);
             await task;
         }
 
@@ -69,6 +111,28 @@ namespace easyAsyncCancel
 
 
         /// <summary>
+        /// add cancellation functionality to Task T with exception message
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="milliseconds"></param>
+        /// <param name="message"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="OperationCanceledException"></exception>
+        public static async Task<T> CancelAfter<T>(
+            this Task<T> task, int milliseconds,string message)
+        {
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(milliseconds);
+            var tcs = new TaskCompletionSource<bool>();
+            using (cts.Token.Register(
+                s => ((TaskCompletionSource<bool>) s).TrySetResult(true), tcs))
+                if (task != await Task.WhenAny(task, tcs.Task))
+                    throw new OperationCanceledException(message,cts.Token);
+            return await task;
+        }
+        
+        /// <summary>
         /// add cancellation functionality to Task
         /// </summary>
         /// <param name="task"></param>
@@ -89,11 +153,27 @@ namespace easyAsyncCancel
             await task;
         }
 
-        public static CancellationToken TimedToken(this Task task, int milliseconds)
+
+        /// <summary>
+        /// add cancellation functionality to Task with exception message
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="milliseconds"></param>
+        /// <param name="message"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="OperationCanceledException"></exception>
+        public static async Task CancelAfter(
+            this Task task, int milliseconds,string message)
         {
             var cts = new CancellationTokenSource();
-            cts.CancelAfter(2000);
-            return cts.Token;
+            cts.CancelAfter(milliseconds);
+            var tcs = new TaskCompletionSource<bool>();
+            using (cts.Token.Register(
+                s => ((TaskCompletionSource<bool>) s).TrySetResult(true), tcs))
+                if (task != await Task.WhenAny(task, tcs.Task))
+                    throw new OperationCanceledException(message,cts.Token);
+            await task;
         }
     }
 }
